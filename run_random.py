eb41db386e773f37a802d8c28f9844b89da819ea
Nick Graham <nickbgraham83@gmail.com>
2017-09-26 12:32:54 -0500

    Incorporatingmodel

--- run_random.py
+++ run_random.py
@@ -1,10 +1,17 @@
 import gym
 import myplot
-import numpy
+import numpy as np
 
 render = False
 n_episodes=1
 
+n_hidden = 500
+n_actions = 3
+dim = 96*96
+
+model = {}
+model['W1'] = np.random.rand(n_hidden, dim)
+model['W2'] = np.random.rand(n_hidden, n_actions)
 
 def main():
     env = gym.make('CarRacing-v0')
@@ -21,6 +28,11 @@ def main():
             observation, reward, done, info = env.step(action) # observation is 96x96x3
             sum_reward += reward
 
+            if (t==233):
+                o = preproc(observation)
+                t = cnn(o)
+                print(t)
+                break
             if(t % 5 == 0):
                 preproc(observation)
             if (t % 100 == 0):
@@ -37,27 +49,31 @@ def main():
 
 
 def preproc(obs):
-    zero = numpy.array([0,0,0])
-    other = numpy.array([102,204,102])
-    other2 = numpy.array([102,229,102])
-
+    zero = np.array([0,0,0])
+    other = np.array([102,204,102])
+    other2 = np.array([102,229,102])
 
     new_list = []
     for col in obs:
       for i in range(len(col)):
-        if numpy.array_equal(col[i], zero):
+        if np.array_equal(col[i], zero):
           new_list.append(0)
-        elif numpy.array_equal(col[i], other):
+        elif np.array_equal(col[i], other):
             new_list.append(1)
-        elif numpy.array_equal(col[i], other2):
+        elif np.array_equal(col[i], other2):
             new_list.append(2)
         else:
             print(col[i])
             new_list.append(-1)
 
-    b = numpy.array(new_list).reshape(96,96)
+    b = np.array(new_list)
     return b
 
+def cnn(obs):
+    one = np.dot(model['W1'], obs)
+    one[one < 0] = 0 #ReLu non linearity
+    two = np.dot(model['W2'], one)
+    return two
 
 if __name__ == "__main__":
     main()
