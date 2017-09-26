caa66e768a8c91a337d44e8f679cb32d106d0c6a
Nick Graham <nickbgraham83@gmail.com>
2017-09-25 22:29:46 -0500

    Adds comment describing observation

--- run_random.py
+++ run_random.py
@@ -2,17 +2,22 @@ import gym
 import myplot
 
 render = False
-
+n_episodes=1
 env = gym.make('CarRacing-v0')
 
 rewards=[]
-for i_episode in range(21):
+for i_episode in range(n_episodes):
     observation = env.reset()
     sum_reward = 0
     for t in range(1000):
         if render: env.render()
         action = env.action_space.sample() # [steering, gas, brake]
-        observation, reward, done, info = env.step(action)
+        observation, reward, done, info = env.step(action) # observation is 96x96x3
+        print(len(observation))
+        print(len(observation[0]))
+        print(len(observation[0][0]))
+        print(observation)
+        break
         sum_reward += reward
         if(t%100 == 0): print(t)
         if done or t==999:
@@ -22,5 +27,5 @@ for i_episode in range(21):
         if done:
             break;
 
-print (rewards)
-myplot.plotRewards("Random", rewards)
+#print (rewards)
+myplot.plotRewards("Random", rewards, 1)
