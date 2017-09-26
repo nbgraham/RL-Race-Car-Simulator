38b74d5629bc6b9c37d9fe43c2605ada9895d33b
Nick Graham <nickbgraham83@gmail.com>
2017-09-26 12:49:55 -0500

    Cnn outputs values for actions (basically random)

--- run_random.py
+++ run_random.py
@@ -10,8 +10,8 @@ n_actions = 3
 dim = 96*96
 
 model = {}
-model['W1'] = np.random.rand(n_hidden, dim)
-model['W2'] = np.random.rand(n_hidden, n_actions)
+model['W1'] = np.random.randn(n_hidden, dim)
+model['W2'] = np.random.randn(n_actions, n_hidden)
 
 def main():
     env = gym.make('CarRacing-v0')
@@ -33,8 +33,6 @@ def main():
                 t = cnn(o)
                 print(t)
                 break
-            if(t % 5 == 0):
-                preproc(observation)
             if (t % 100 == 0):
                 print(t)
             if done or t==999:
@@ -45,26 +43,32 @@ def main():
                 break;
 
     #print (rewards)
-    myplot.plotRewards("Random", rewards, 1)
+    #myplot.plotRewards("Random", rewards, 1)
 
 
 def preproc(obs):
-    zero = np.array([0,0,0])
-    other = np.array([102,204,102])
-    other2 = np.array([102,229,102])
+    black = np.array([0,0,0])
+    green = np.array([102,204,102])
+    light_green = np.array([102,229,102])
+    grey1 = np.array([107,107,107])
+    grey2 = np.array([105,105,105])
 
     new_list = []
     for col in obs:
       for i in range(len(col)):
-        if np.array_equal(col[i], zero):
-          new_list.append(0)
-        elif np.array_equal(col[i], other):
+        if np.array_equal(col[i], black):
             new_list.append(1)
-        elif np.array_equal(col[i], other2):
+        elif np.array_equal(col[i], green):
             new_list.append(2)
+        elif np.array_equal(col[i], light_green):
+            new_list.append(2.1)
+        elif np.array_equal(col[i], grey1):
+            new_list.append(3)
+        elif np.array_equal(col[i], grey2):
+            new_list.append(3)
         else:
             print(col[i])
-            new_list.append(-1)
+            new_list.append(0)
 
     b = np.array(new_list)
     return b
@@ -73,7 +77,17 @@ def cnn(obs):
     one = np.dot(model['W1'], obs)
     one[one < 0] = 0 #ReLu non linearity
     two = np.dot(model['W2'], one)
+
+    two[0] = posNorm(two[0]) #brake
+    two[1] = posNorm(two[1]) #gas
+    two[2] = posNegNorm(two[2]) #steering
     return two
+
+def posNorm(x):
+    return 1.0 / (1.0 + np.exp(-x)) # sigmoid squashing
+
+def posNegNorm(x):
+    return 2.0 / (1.0 + np.exp(-x)) - 1.0 # sigmoid squashing
 
 if __name__ == "__main__":
     main()
