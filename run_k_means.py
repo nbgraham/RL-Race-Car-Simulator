f203418ab1b98e5a075307cda040e7fc49972e4f
Nick Graham <nickbgraham83@gmail.com>
2017-10-06 13:49:23 -0500

    Adds q learning

--- run_k_means.py
+++ run_k_means.py
@@ -17,56 +17,63 @@ batch_size = 10
 
 target_reward_per_frame = 1.5
 
+action_set = np.array([
+#nothing
+[0,0,0],
+#steering
+[-1.0,0,0],
+[-0.5,0,0],
+[0,0,0],
+[0.5,0,0],
+[1.0,0,0],
+#gas
+[0,0.5,0],
+[0,0.8,0],
+#brake
+[0,0,0.5],
+[0,0,0.8]
+])
+
+
 def main(k):
     f = open('road_means_' + str(k) + '.npy','rb')
     road_means = np.load(f)
-    train(road_means)
+    train(road_means, 0.001, 0.99)
 
 
-def train(road_means):
+def train(road_means, alpha, discount):
     env = gym.make('CarRacing-v0')
     action = [0,0.1,0]
+
+    state_action_value = {}
 
     rewards=[]
     for i_episode in range(n_episodes):
         observation = env.reset()
         sum_reward = 0
         interval_reward = 0
+        prev_state = None
+        prev_aciton = None
 
         for t in range(max_time_steps):
             if render: env.render()
 
             if t> 0 and (t % action_time_steps == 0):
+                state = get_state(observation, road_means)
                 action = env.action_space.sample() # [steering, gas, brake]
 
-                road_matrix = pre. cropped_grayscale_car_road(observation)
-                dashboard_values = pre.get_dashboard_values(observation)
-                steering = dashboard_values[0]
-                speed = dashboard_values[1]
+                if prev_state is not None and prev_action is not None:
+                    if str(prev_state) not in state_action_value:
+                        state_action_value[str(prev_state)] = {}
+                    state_action_value[str(prev_state)][str(prev_action)] = q_update(prev_state, state, prev_action, state_action_value, alpha, interval_reward, discount)
 
-                speed_state = 0
-                if speed > 0.4:
-                    speed_state = 1
+                prev_state = state
+                prev_action = action
 
-                steering_state = 0
-                if steering < 0.3:
-                    steering_state = -1
-                elif steering > 0.7:
-                    steering_state = 1
-
-                min_dif = 10**10
-                road_state = -1
-
-                for i_road_mean in range(len(road_means)):
-                    dif = np.sum(abs(road_matrix - road_means[i_road_mean]))
-                    if dif < min_dif:
-                        min_dif = dif
-                        road_state = i_road_mean
-
-                cv2.imshow('road state', road_means[road_state])
+                cv2.imshow('road state', road_means[state[2]])
                 cv2.waitKey(1)
 
-                print("Road: {} Steering: {} Speed: {}".format(road_state, steering_state, speed_state))
+                print(str(state))
 
                 interval_reward = 0
 
@@ -88,6 +95,77 @@ def train(road_means):
     f.close()
 
     myplot.plotRewards("Random", rewards, int(n_episodes/10))
+
+
+def q_update(prev_state, next_state, prev_action, state_action_value, alpha, reward, discount, init=0):
+    learned_value = reward + discount * get_action_max(next_state, state_action_value)
+    old_value = get_q_value(prev_state, prev_action, state_action_value)
+    return old_value + alpha * (learned_value - old_value)
+
+
+def get_action_max(state, state_action_value, init=0):
+    value = init
+    if state in state_action_value:
+        max_value = -1*10**10
+        for action in action_set:
+            value = init
+            if action in state_action_value[state]:
+                value =  state_action_value[state][action]
+            if value > max_value:
+                max_value = value
+                action = action
+
+    return value
+
+
+def get_q_value(state, action, state_action_value, init=0):
+    value = init
+    if state in state_action_value:
+        if action in state_action_value[state]:
+            value = state_action_value[state][action]
+
+    return value
+
+
+def get_state(observation, road_means):
+    road_matrix = pre. cropped_grayscale_car_road(observation)
+    dashboard_values = pre.get_dashboard_values(observation)
+    steering = dashboard_values[0]
+    speed = dashboard_values[1]
+
+    speed_state = get_speed_state(speed)
+    steering_state = get_steering_state(steering)
+    road_state = get_road_state(road_matrix, road_means)
+
+    state = (speed_state, steering_state, road_state)
+    return state
+
+def get_speed_state(speed):
+    speed_state = 0
+    if speed > 0.4:
+        speed_state = 1
+    return speed_state
+
+
+def get_steering_state(steering):
+    steering_state = 0
+    if steering < 0.3:
+        steering_state = -1
+    elif steering > 0.7:
+        steering_state = 1
+    return steering_state
+
+
+def get_road_state(road_matrix, road_means):
+    min_dif = road_matrix.size**2
+    road_state = -1
+
+    for i_road_mean in range(len(road_means)):
+        dif = np.sum(abs(road_matrix - road_means[i_road_mean]))
+        if dif < min_dif:
+            min_dif = dif
+            road_state = i_road_mean
+    return road_state
 
 
 def show_model(model):
