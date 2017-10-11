9d7df8e9b9376110244a3a3c177fe023f6b9f9ce
Nick Graham <nickbgraham83@gmail.com>
2017-10-11 09:34:37 -0500

    Functions for plotting reward

--- after_plot.py
+++ after_plot.py
@@ -2,7 +2,19 @@ import numpy as np
 import json
 import myplot
 
-f = open('luc/rewards.npy', 'rb') #open('rewards/rewards')
-rewards = np.load(f) #json.load(f)
 
-myplot.plotRewards("Luc Prieur's Deep-Q Learning",rewards,100)
+def plot_luc():
+    f = open('luc/rewards.npy', 'rb')
+    #open('rewards/rewards')
+    rewards = np.load(f) #json.load(f)
+
+    myplot.plotRewards("Luc Prieur's Deep-Q Learning",rewards,100)
+
+def plot_k_means(k):
+    f = open('rewards/reward_' + str(k) + '.json' )
+    rewards = json.load(f)
+
+    myplot.plotRewards("K=" + str(k) + " means Q Learning",rewards,100)
+
+if __name__ == "__main__":
+    plot_k_means(20)
