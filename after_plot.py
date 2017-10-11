9b321c015f9d26f0218161230b5168afe5cde2d2
Nick Graham <nickbgraham83@gmail.com>
2017-10-11 09:30:48 -0500

    Plot npy rewards

--- after_plot.py
+++ after_plot.py
@@ -1,7 +1,8 @@
+import numpy as np
 import json
 import myplot
 
-f = open('rewards/rewards')
-rewards = json.load(f)
+f = open('luc/rewards.npy', 'rb') #open('rewards/rewards')
+rewards = np.load(f) #json.load(f)
 
-myplot.plotRewards("Simple CNN",rewards,10)
+myplot.plotRewards("Luc Prieur's Deep-Q Learning",rewards,100)
