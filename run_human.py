f8d368d81e5492a0f9a3562dc127a6548aec8cb3
andrew <sandshrew@users.noreply.github.com>
2017-10-21 15:06:05 -0500

    adds preprocessing to human to see state output

--- run_human.py
+++ run_human.py
@@ -1,9 +1,11 @@
 from pyglet.window import key
 import gym
 import numpy as np
+import preprocessing as pre
+
 
 record_video = False
-plays = 1
+plays = 10
 
 
 
@@ -32,19 +34,27 @@ env.env.viewer.window.on_key_press = key_press
 env.env.viewer.window.on_key_release = key_release
 
 for i in range(plays):
-    env.reset()
+    observation = env.reset()
     total_reward = 0.0
     steps = 0
     restart = False
+    s = None
     while True:
+        if steps == 0:
+            state = pre.compute_state(observation)
+        print("state\n",state)
+        print("action\n", a)
         s, r, done, info = env.step(a)
+        state = pre.compute_state(s)
+        print("reward\n",r)
+        print("next state\n", state)
         total_reward += r
         if steps % 200 == 0 or done:
             print("\naction " + str(["{:+0.2f}".format(x) for x in a]))
             print("step {} total_reward {:+0.2f}".format(steps, total_reward))
-            #import matplotlib.pyplot as plt
-            #plt.imshow(s)
-            #plt.savefig("test.jpeg")
+            # import matplotlib.pyplot as plt
+            # plt.imshow(s)
+            # plt.savefig("test.jpeg")
         steps += 1
         if not record_video: # Faster, but you can as well call env.render() every time to play full window.
             env.render()
