280a571624f11e105cc7f107d6aa964d2b1d494b
Nick Graham <nickbgraham83@gmail.com>
2017-10-25 10:35:21 -0500

    Convert sarsa to new base model and don't update

--- sarsa/model.py
+++ sarsa/model.py
@@ -7,27 +7,33 @@ import numpy as np
 import random
 
 from sarsa.hyperparameters import gamma, alpha
-from std_q.model import Model as StdQModel
+from base.model import BaseModel
 
-class Model(StdQModel):
+class Model(BaseModel):
+    def __init__(self, env, name, input_size, action_set):
+        BaseModel.__init__(self, env, name, input_size, action_set, alpha=alpha, gamma=gamma)
+    
     def get_action(self, state, eps, reward):
         argmax_qvals, qvals = self.sample_action(state, eps)
 
-        action = Model.convert_argmax_qval_to_env_action(argmax_qvals)
+        action = self.action_set[argmax_qvals]
         change = 0
 
-        if self.prev_state is not None and self.prev_qvals is not None and self.prev_argmax is not None:
-            G = reward + gamma * qvals[argmax_qvals]
-            y = self.prev_qvals[:]
-            change = G - y[self.prev_argmax]
-            y[self.prev_argmax] += alpha * change
-            self.update(self.prev_state, y)
+        if self.prev_state is not None and self.prev_net_output is not None and self.prev_action_index is not None:
+            G = reward + self.gamma * qvals[argmax_qvals]
+            y = self.prev_net_output[:]
+            change = G - y[self.prev_action_index]
+            y[self.prev_action_index] += alpha * change
+            # self.update(self.prev_state, y)
 
         self.prev_state = state
-        self.prev_qvals = qvals
-        self.prev_argmax = argmax_qvals
+        self.prev_net_output = qvals
+        self.prev_action_index = argmax_qvals
 
         loss = change ** 2
         return action, loss
 
         return action
+    
+    def get_action_selection_parameter(cur_episode, total_episodes):
+        return 0
